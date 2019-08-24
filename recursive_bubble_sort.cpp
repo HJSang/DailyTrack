04f51417ca4b661f10efb98dd373a836124dc6d9
Hejian <hjsang@iastate.edu>
2019-08-24 12:41:00 -0700

    recursive bubble sort

---
+++ recursive_bubble_sort.cpp
@@ -0,0 +1,48 @@
+/* CPP Program to implement Recursive Bublle Sorting Algorithm*/
+#include<iostream>
+using namespace std;
+
+// swap 
+template <class T>
+void swap(T *x, T *y)
+{
+	T temp = *y;
+	*y = *x;
+	*x = temp;
+}
+
+// bubble sorting
+template <class T>
+void bubble_sorting(T arr[], int n)
+{
+	if(n==1) return;
+	for(int i=0; i<n-1; i++){
+		if(arr[i]>arr[i+1])
+		{
+			swap(&arr[i], &arr[i+1]);
+		}
+	}
+	return bubble_sorting(arr, n-1);
+}
+
+// print array
+template <class T>
+void print_array(T arr[], int n)
+{
+	for(int i=0; i<n; i++)
+	{
+		cout<< arr[i]<<endl;
+	}
+}
+
+// driver code
+int main()
+{
+	int arr[] = {64, 34, 25, 12, 22, 11, 90};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	cout<<"Before Sorting:"<<endl;
+	print_array(arr,n);
+	cout<<"After Sorting:"<<endl;
+	bubble_sorting(arr,n);
+	print_array(arr,n);
+}
